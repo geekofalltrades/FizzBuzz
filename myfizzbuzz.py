a8635c40e2328226ae635dc2b81caecd7a583b75
geekofalltrades <thegeekofalltrades@gmail.com>
2014-02-13 11:01:15 -0800

    Initial solution - print Fizz and Buzz and FizzBuzz when appropriate.

--- myfizzbuzz.py
+++ myfizzbuzz.py
@@ -1,5 +1,11 @@
-def fizzbuzz():
-    pass
+def fizzbuzz(n):
+    solution = ''
+    if not n % 3:
+        solution += 'Fizz'
+    if not n % 5:
+        solution += 'Buzz'
+    return solution
 
 if __name__ == '__main__':
-    pass
+    num = int(input("Enter an integer."))
+    print(fizzbuzz(num))
