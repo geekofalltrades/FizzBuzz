1c544804ddfd813dc8bc80986b2d471eae9f0a1a
geekofalltrades <thegeekofalltrades@gmail.com>
2014-02-13 11:31:32 -0800

    New fizzbuzz function is working in simple tests.

--- myfizzbuzz.py
+++ myfizzbuzz.py
@@ -3,7 +3,16 @@ def fizzbuzz(n, **kwargs):
        kwargs, where keys are strings to be printed and values are
        numbers by which n must be divisible to print the key string.
     """
-    pass
+    tests = sorted(kwargs.iteritems(), key=lambda x: x[1])
+    print(tests)
+
+    solution = ''
+    for test in tests:
+        string, num = test
+        if not n % num:
+            solution += string
+
+    return solution if solution else n
 
 def oldfizzbuzz(n):
     """Classic fizzbuzz solution: prints Fizz if n divisible by 3, Buzz
@@ -20,4 +29,14 @@ def oldfizzbuzz(n):
 
 if __name__ == '__main__':
     num = int(input("Enter an integer.\n"))
-    print(fizzbuzz(num))
+    tests = {}
+    userin = 'blargh'
+    while(userin):
+        userin = raw_input(
+"""Enter tests, where term and integer to be divided by are separated by
+whitespace (as in 'Fizz 3' or 'Buzz   5'). Submit a blank line when finished.\n""")
+        if not userin:
+            break
+        terms = userin.split()
+        tests[terms[0]] = int(terms[1])
+    print(fizzbuzz(num, **tests))
