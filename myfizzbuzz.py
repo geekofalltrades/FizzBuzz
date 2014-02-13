5b573f6ecc3fc1856b90585651b89213be00d52f
geekofalltrades <thegeekofalltrades@gmail.com>
2014-02-13 11:38:28 -0800

    Fixed docstrings.

--- myfizzbuzz.py
+++ myfizzbuzz.py
@@ -1,8 +1,8 @@
 def fizzbuzz(n, **kwargs):
     """Extensible fizzbuzz function, passed a number and a list of
-       kwargs, where keys are strings to be printed and values are
-       numbers by which n must be divisible to print the key string.
-    """
+kwargs, where keys are strings to be printed and values are
+numbers by which n must be divisible to print the key string.
+"""
     tests = sorted(kwargs.iteritems(), key=lambda x: x[1])
 
     solution = ''
@@ -15,9 +15,9 @@ def fizzbuzz(n, **kwargs):
 
 def oldfizzbuzz(n):
     """Classic fizzbuzz solution: prints Fizz if n divisible by 3, Buzz
-       if divisible by 5, FizzBuzz if divisible by both, returns the
-       number if divisible by neither.
-    """
+if divisible by 5, FizzBuzz if divisible by both, returns the
+number if divisible by neither.
+"""
 
     solution = ''
     if not n % 3:
