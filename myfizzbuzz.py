35da246cd73488cfc6d2cdf801a0b24c662335a9
geekofalltrades <thegeekofalltrades@gmail.com>
2014-02-13 11:08:37 -0800

    Adds prototype for extensible fizzbuzz, moves old fizzbuzz to oldfizzbuzz, adds docstrings to each function.

--- myfizzbuzz.py
+++ myfizzbuzz.py
@@ -1,4 +1,16 @@
-def fizzbuzz(n):
+def fizzbuzz(n, **kwargs):
+    """Extensible fizzbuzz function, passed a number and a list of
+       kwargs, where keys are strings to be printed and values are
+       numbers by which n must be divisible to print the key string.
+    """
+    pass
+
+def oldfizzbuzz(n):
+    """Classic fizzbuzz solution: prints Fizz if n divisible by 3, Buzz
+       if divisible by 5, FizzBuzz if divisible by both, returns the
+       number if divisible by neither.
+    """
+
     solution = ''
     if not n % 3:
         solution += 'Fizz'
